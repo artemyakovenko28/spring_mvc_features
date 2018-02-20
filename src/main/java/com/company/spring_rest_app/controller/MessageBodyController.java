4c6f55afa3c63b96224d2f22521aa19ca9fb0104
artemyakovenko <jeep2801@gmail.com>
2018-02-20 16:39:05 +0200

    HTTP Message Body handling using @RequestBody and @ResponseBody (MessageBodyController), relevant tests

---
+++ src/main/java/com/company/spring_rest_app/controller/MessageBodyController.java
@@ -0,0 +1,33 @@
+package com.company.spring_rest_app.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+
+@Controller
+public class MessageBodyController {
+
+    @RequestMapping(value = "/message", method = RequestMethod.GET,
+    consumes = MediaType.TEXT_PLAIN_VALUE, produces = MediaType.TEXT_HTML_VALUE)
+    @ResponseBody
+    public String handleGetRequest(@RequestBody byte[] bytes, @RequestBody String str) {
+        System.out.println("Request body in bytes: " + Arrays.toString(bytes));
+        System.out.println("Request body in String" + str);
+        return "<html>" +
+                "<body>" +
+                "<h1>Hi there</h1>" +
+                "</body>" +
+                "</html>";
+    }
+
+    @RequestMapping(value = "/message", method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public void handleFormRequest(@RequestBody MultiValueMap<String, String> formParams) {
+        System.out.println("Form params received: " + formParams);
+    }
+}
---
+++ src/test/java/com/company/spring_rest_app/controller/MessageBodyControllerTest.java
@@ -0,0 +1,65 @@
+package com.company.spring_rest_app.controller;
+
+import com.company.spring_rest_app.config.WebConfig;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = WebConfig.class)
+public class MessageBodyControllerTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        DefaultMockMvcBuilder builder = MockMvcBuilders.webAppContextSetup(this.wac);
+        this.mockMvc = builder.build();
+    }
+
+    @Test
+    public void handleGetRequestTest() throws Exception {
+        ResultMatcher ok = MockMvcResultMatchers.status()
+                .isOk();
+
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/message")
+                .contentType(MediaType.TEXT_PLAIN)
+                .content("test message");
+        this.mockMvc.perform(builder)
+                .andExpect(ok)
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    public void handleFormRequestTest() throws Exception {
+        ResultMatcher ok = MockMvcResultMatchers.status()
+                .isOk();
+
+        MockHttpServletRequestBuilder builder =
+                MockMvcRequestBuilders.post("/message")
+                        .contentType("application/x-www-form-urlencoded")
+                        //use param instead of content
+                        .param("name", "joe");
+
+        this.mockMvc.perform(builder)
+                .andExpect(ok);
+    }
+}
