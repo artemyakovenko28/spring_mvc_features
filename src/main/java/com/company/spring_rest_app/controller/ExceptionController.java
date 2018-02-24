3c01eac140802bd32bfb17bdbe5714580810bd59
artemyakovenko <jeep2801@gmail.com>
2018-02-24 11:50:15 +0200

    Annotation based exception handling using @ExceptionHandler (ExceptionController)

--- src/main/java/com/company/spring_rest_app/controller/ExceptionController.java
+++ src/main/java/com/company/spring_rest_app/controller/ExceptionController.java
@@ -1,7 +1,13 @@
 package com.company.spring_rest_app.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Arrays;
 
 @Controller
 public class ExceptionController {
@@ -19,5 +25,17 @@ public class ExceptionController {
     @RequestMapping("/exception/arithmetic")
     public void throwArithmeticException() {
         throw new ArithmeticException("Test ArithmeticException");
+    }
+
+    @RequestMapping("/exception/class-cast")
+    public void throwClassCastException() {
+        throw new ClassCastException();
+    }
+
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ExceptionHandler(ClassCastException.class)
+    @ResponseBody
+    public String handleException(ClassCastException ex) {
+        return "Exception: " + ex + "\nStack trace: " + Arrays.toString(ex.getStackTrace());
     }
 }
